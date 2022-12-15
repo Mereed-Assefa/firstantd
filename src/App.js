36fa82de3241c10f9e00b9562378866d594bda2c
Mereed Assefa <mrdassefa@gmail.com>
2022-12-15 16:34:00 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Myform from './Myform';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Myform></Myform>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Mytable.js
@@ -0,0 +1,13 @@
+import {Table} from 'antd'
+
+function Mytable() {
+  return (
+    <div className="Mytable">
+      <header className="Mytable-header">
+      
+      </header>
+    </div>
+  );
+}
+
+export default Mytable;
